2d4f8af49b2dbcd5cb1d600b626da9e74a8b3613
Masha <zaytseva.m@husky.neu.edu>
2019-09-01 16:59:21 +0300

    add yandex maps to home page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,12 +1,37 @@
 import React from "react";
 import "../css/Contacts.css";
 import map from '../images/map.png';
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+
+
+const mapData = {
+  center:[55.73557716284752, 37.45359357672123],
+  zoom: 16,
+};
+
+const coordinates =
+  [55.73557716284752, 37.45359357672123];
+
+const placemarkStyle = {
+  iconColor: "#C95501"
+}
+
+var mapStyle = {
+  width: '100%',
+  height: '100%',
+};
 
 function Contacts() {
   return (
     <section id="contacts" class="row">
     <div id="contact-map" class="col-12 col-lg-6">
-    <img id="map" src={map} />
+    <div id="map">
+    <YMaps >
+    <Map style={mapStyle} width="100%" height="100%" defaultState={mapData}>
+      {<Placemark options={placemarkStyle} geometry={coordinates} />}
+    </Map>
+  </YMaps>
+  </div>
     </div>
     <div id="contact-form-outer" class="col-12 col-lg-6">
       <h2 class="contact-form-header large-size-text">
